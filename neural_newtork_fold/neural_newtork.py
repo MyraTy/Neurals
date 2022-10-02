8213229d7c7db2cd9189cec0fdcecd8704cc6a83
MyraTy <113852108+MyraTy@users.noreply.github.com>
2022-10-02 17:39:24 +0200

    Update

--- neural_newtork_fold/neural_newtork.py
+++ neural_newtork_fold/neural_newtork.py
@@ -1,25 +1,32 @@
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
-import synteticer as sc
+import tensorflow_shortcuts as ts
 
 print("Initialising...")
-trainingI = sc.cdata([
+trainingI = ts.cdata([
     [[[[[1.0]]]], [[[[2.0]]]]],
     [[[[[3.0]]]], [[[[4.0]]]]]], type=float)
-trainingO = sc.cdata([
-    [[[[[1.0,]]]], [[[[11.0]]]]],
-    [[[[[21.0]]]], [[[[1211.0]]]]]], type=float)
+trainingO = ts.cdata([
+    [[[[[1.0]]]], [[[[4.0]]]]],
+    [[[[[9.0]]]], [[[[61.0]]]]]], type=float)
 tf.random.set_seed(42)
 
 print("Creating Neurals...")
 seq = keras.Sequential([keras.layers.Dense(units=6, input_shape=[2, 1, 1, 1, 1], activation="relu")])
 print("Compiling...")
-seq.compile(optimizer=keras.optimizers.Adam(1), loss='mean_squared_error')
+seq.compile(optimizer=keras.optimizers.Adam(1.5), loss='mean_squared_error')
 
 print("Training...")
-seq.fit(trainingI, trainingO, epochs=11000)
+seq.fit(trainingI, trainingO, epochs=1000, verbose=False)
 print("Done!")
-print("Enter a value to make Neurals predict it!")
-res = sc.predict(seq, [[[[[[float(input())]]]]], [[[[[float(input())]]]]]])
-print(res[3])
+print("Enter a float value to make Neurals predict it!")
+try:
+    res = ts.predict(seq, [float(input())], Type=float)
+    print(res)
+except ValueError:
+    raise ValueError("That's not a float value. If you typed well, please ask MyraTy about this.")
+except Exception:
+    raise Exception("An error on source code ocured. Please ask MyraTy about this.")
+finally:
+    print("Program finished.")
